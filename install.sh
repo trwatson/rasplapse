9b0fb28fb740b97deed977ef23adf8d80e78db33
Troy Watson <troseph@yahoo.com>
2021-06-01 13:35:37 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  echo "sudo ${0}"
+  exit
+fi
 mkdir -p /opt/camerad/DCIM/ /opt/camerad/bin/
 chown -R pi:pi /opt/camerad/
 cp camerad /etc/init.d/camerad
 cp camerad.service /etc/systemd/system/camerad.service
 cp camerad.sh /opt/camerad/bin/camerad.sh
+chmod +x /opt/camerad/bin/camerad.sh
 systemctl start camerad.service && echo "service started photos will be saved to /opt/camerad/DCIM/" || echo "something went wrong with installation"
