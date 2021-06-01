8b36570fe127137c12655cd88dee0ecb8b8c7d1c
Troy Watson <troseph@yahoo.com>
2021-06-01 11:10:28 -0600

    Create camerad.sh

---
+++ camerad.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          AutmaticCamera
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: Takes pictures...
+### END INIT INFO
+# one second default interval if not specified as first arguement
+DEFAULT_INTERVAL=1
+if [[ ${1} ]]; then
+  INTERVAL=${1}
+else
+  INTERVAL=${DEFAULT_INTERVAL}
+fi
+#make the dir
+mkdir -p /opt/DCIM
+echo "Started Camerd at $(date +%s)" > /dev/kmsg
+while true
+do
+  raspistill -o /opt/DCIM/IMG_$(date +%s).jpg
+  sleep ${INTERVAL}
+done
